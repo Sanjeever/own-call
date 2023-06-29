afbad85e39a6a5b3acbee335c4009dc2b1a9a8ac
Sanjeever <78210374+Sanjeever@users.noreply.github.com>
2023-06-29 14:12:12 +0800

    🦄 refactor(test): update test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,12 +1,12 @@
 import { test, expectTypeOf, expect } from 'vitest'
 import { add } from '.'
 
-const ret = add.ownCall({ x: 10 }, 5) // ret = 15
-
+const _this = { x: 10 }
 test('ownCall()', () => {
-  expect(ret).toBe(15)
+  expect(add.ownCall(_this, 5)).toBe(add.call(_this, 5))
 })
 
 test('ownCall() types work properly', () => {
+  const ret = add.ownCall(_this, 5) // ret = 15
   expectTypeOf(ret).toBeNumber()
 })
